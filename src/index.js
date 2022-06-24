28a81c10d6d50b0d51822b614ec06940a3e06dcc
BrandonC123 <brandoc318@gmail.com>
2022-06-24 00:13:35 -0700

    Refactor game loop.

--- src/index.js
+++ src/index.js
@@ -32,17 +32,7 @@ const GameBoard = () => {
         [[], [], [], [], [], [], [], [], [], []],
         [[], [], [], [], [], [], [], [], [], []],
     ];
-    let shipList = [
-        {
-            name: "destroyer",
-            sunk: true,
-            placement: [
-                [4, 3],
-                [4, 4],
-                [4, 5],
-            ],
-        },
-    ];
+    let shipList = [];
     let missedAttackList = [];
     return {
         gameBoardArray,
@@ -78,6 +68,9 @@ const GameBoard = () => {
                         });
                     }
                     shipList[index].hitList.push(coordinate);
+                    if (shipList[index].isSunk()) {
+                        shipList[index].sunk = true;
+                    }
                     console.log(shipList);
                     return true;
                 }
@@ -112,6 +105,7 @@ function player(name, playerGameBoard) {
     return {
         name: name,
         playerGameBoard: playerGameBoard,
+        generateAttack,
     };
 }
 
@@ -142,9 +136,9 @@ const displayHandler = (() => {
             enemyCells[i].addEventListener("click", () => {
                 console.log(i);
                 const coordinate = [Math.floor(i / 10 + 1), (i % 10) + 1];
-                console.log(coordinate);
                 player2.playerGameBoard.receiveAttack(coordinate);
                 console.log(player2);
+                gameHandler.gameLoop();
             });
         }
     }
@@ -155,79 +149,53 @@ const displayHandler = (() => {
     };
 })();
 
-const gameLoop = (() => {
-    function newGame(player1Name, player2Name) {
-        let player1 = player(player1Name, GameBoard());
-        let player2 = player(player2Name, GameBoard());
+const gameHandler = (() => {
+    let player1Name = "brandon";
+    let player2Name = "ai";
+    let player1 = player(player1Name, GameBoard());
+    let player2 = player(player2Name, GameBoard());
 
-        displayHandler.generateGameBoard("brandon");
-        displayHandler.generateGameBoard("ai");
-        player1.playerGameBoard.placeShip(
-            player1Name,
-            ship(5, [], false),
-            [4, 2]
-        );
-        player1.playerGameBoard.placeShip(
-            player1Name,
-            ship(4, [], false),
-            [1, 1]
-        );
-        player1.playerGameBoard.placeShip(
-            player1Name,
-            ship(4, [], false),
-            [8, 5]
-        );
-        player1.playerGameBoard.placeShip(
-            player1Name,
-            ship(3, [], false),
-            [6, 2]
-        );
-        player1.playerGameBoard.placeShip(
-            player1Name,
-            ship(2, [], false),
-            [3, 7]
-        );
+    displayHandler.generateGameBoard("brandon");
+    displayHandler.generateGameBoard("ai");
+    displayHandler.addAttackEventListener(player2);
 
-        player2.playerGameBoard.placeShip(
-            player2Name,
-            ship(5, [], false),
-            [4, 2]
-        );
-        player2.playerGameBoard.placeShip(
-            player2Name,
-            ship(4, [], false),
-            [1, 1]
-        );
-        player2.playerGameBoard.placeShip(
-            player2Name,
-            ship(4, [], false),
-            [8, 5]
-        );
-        player2.playerGameBoard.placeShip(
-            player2Name,
-            ship(3, [], false),
-            [6, 2]
-        );
-        player2.playerGameBoard.placeShip(
-            player2Name,
-            ship(2, [], false),
-            [3, 7]
-        );
-        displayHandler.addAttackEventListener(player2);
+    player1.playerGameBoard.placeShip(player1Name, ship(5, [], false), [4, 2]);
+    player1.playerGameBoard.placeShip(player1Name, ship(4, [], false), [1, 1]);
+    player1.playerGameBoard.placeShip(player1Name, ship(4, [], false), [8, 5]);
+    player1.playerGameBoard.placeShip(player1Name, ship(3, [], false), [6, 2]);
+    player1.playerGameBoard.placeShip(player1Name, ship(2, [], false), [3, 7]);
 
+    player2.playerGameBoard.placeShip(player2Name, ship(5, [], false), [4, 2]);
+    player2.playerGameBoard.placeShip(player2Name, ship(4, [], false), [1, 1]);
+    player2.playerGameBoard.placeShip(player2Name, ship(4, [], false), [8, 5]);
+    player2.playerGameBoard.placeShip(player2Name, ship(3, [], false), [6, 2]);
+    player2.playerGameBoard.placeShip(player2Name, ship(2, [], false), [9, 7]);
+
+    let player1Turn = true;
+    function gameLoop() {
+        if (!player1Turn) {
+            let aiAttack = player2.generateAttack();
+            player1.playerGameBoard.receiveAttack(aiAttack)
+            player1Turn = true;
+        }
+        if (player2.playerGameBoard.checkAllShips()) {
+            console.log("game over!");
+        }
+        player1Turn = false;
         console.log(player1);
         console.log(player2);
     }
-    newGame("brandon", "ai");
+    return {
+        gameLoop,
+    };
 })();
 
-let testShip = ship(3, [], false);
-// console.log(testShip)
-let testGameBoard = GameBoard();
+// let testShip = ship(3, [], false);
+// let testGameBoard = GameBoard();
 
-module.exports.hit = testShip.hit;
-module.exports.isSunk = testShip.isSunk;
+// module.exports.hit = testShip.hit;
+// module.exports.isSunk = testShip.isSunk;
 
-module.exports.placeShip = testGameBoard.placeShip;
-module.exports.receiveAttack = testGameBoard.receiveAttack;
-module.exports.checkAllShips = testGameBoard.checkAllShips;
+// module.exports.placeShip = testGameBoard.placeShip;
+// module.exports.receiveAttack = testGameBoard.receiveAttack;
+// module.exports.checkAllShips = testGameBoard.checkAllShips;
